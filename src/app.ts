5b51846d0c71ef262886a5f68714a450fde50f75
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-03-09 19:18:06 -0300

    feat: add a migration test

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,21 @@ dotenv.config();
 import 'reflect-metadata';
 import './shared/infra/typeorm'; // Database configuration
 import express from 'express';
+import User from './modules/user/infra/typeorm/entities/User';
 const app = express();
+
+app.get('/', async (req, res) => {
+  try {
+    const user = new User();
+    user.firstName = 'Timber';
+    user.lastName = 'Saw';
+    user.age = 25;
+    await user.save();
+
+    res.send(await User.find());
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 export default app;
